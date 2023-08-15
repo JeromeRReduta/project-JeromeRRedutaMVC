77b5acfdf5b7b80ff8b3fc36bb485ae257bf8241
Jerome Reduta <JRReduta@yahoo.com>
2023-08-14 20:32:11 -0700

    Cleaned up imports

--- src/main/java/configurations/modular_configs/StemCounterConfig.java
+++ src/main/java/configurations/modular_configs/StemCounterConfig.java
@@ -1,7 +1,6 @@
 package configurations.modular_configs;
 
 import java.io.IOException;
-
 import java.io.Writer;
 import java.nio.file.Path;
 import java.util.Map;
--- src/main/java/data_reading/search_result_indexing/SimpleStemCounterSearcher.java
+++ src/main/java/data_reading/search_result_indexing/SimpleStemCounterSearcher.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
